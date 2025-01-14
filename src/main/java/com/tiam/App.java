a0db6ec00fb579bcecfbc2bca520eb41014c96ac
Tiam-Rietsch <ngnotchouetiam@gmail.com>
2025-01-14 10:38:05 +0100

    resolved class path issues

--- src/main/java/com/tiam/App.java
+++ src/main/java/com/tiam/App.java
@@ -1,18 +1,17 @@
 package com.tiam;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 /**
  * JavaFX App
  */
 public class App extends Application {
-
 
     @Override
     public void start(Stage window) throws IOException {
@@ -27,7 +26,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
